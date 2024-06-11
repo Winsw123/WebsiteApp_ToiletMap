5eceb8221ea4e7a36f5b2a24575b0d7e18008106
lynntsuiplus <lynntsuiplus@gmail.com>
2024-06-11 22:34:49 +0800

    Initial commit for Toilet Project

---
+++ src/main/java/com/example/toilet/ToiletApplication.java
@@ -0,0 +1,13 @@
+package com.example.toilet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToiletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToiletApplication.class, args);
+	}
+
+}

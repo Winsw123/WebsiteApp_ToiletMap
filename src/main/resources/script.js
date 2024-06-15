7e0c4ff3b5139af5df148bd4dd4c3c96110fc0fb
5x55 <reflecstreamz@gmail.com>
2024-06-15 18:39:09 +0800

    finished front end, waiting for back end

--- src/main/resources/script.js
+++ src/main/resources/script.js
@@ -25,6 +25,7 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var markerLayer = L.featureGroup().addTo(map);
 var toiletLayer = L.featureGroup().addTo(map);
+var formLayer = L.featureGroup().addTo(map);
 
 var test = "{\
     \"name\": \"南一中校門口\",\
@@ -39,34 +40,61 @@ var test = "{\
         \"isSoap\" : false\
     }\
 }";
+var test2 = "{\
+    \"values\": [114, 514]\
+}";
+var test3 = [];
 
-var toiletsAll = [];
+var toiletsAll;
 getToiletLocation();
 
 function getToiletLocation() {
-
-    /*TBD: fetch data from database
-    
-    for (var i = 0; i < data.length; i++) {
-        toilets.push(Toilet.set(data[i]));
-    }*/
+    toiletsAll = [];
+    /*let url = ""; // fill url here
+    fetch(url).then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('無法連絡伺服器，請回報開發者');
+        }).then((json) => {
+            var jsonArray = JSON.parse(json);
+            for (var i = 0; i < jsonArray.length; i++) {
+                toiletsAll.push(new Toilet.set(jsonArray[i]));
+            }
+        }).catch((error) => {
+            alert(error);
+        });
+    */
 
     toiletsAll.push(new Toilet()); // test
     toiletsAll.push(new Toilet().set(JSON.parse(test))); // test
     displayToilet(toiletsAll, null);
 }
-
 function getDistance() {
-    var distance = [];
-    /*TBD: calculate distance in back end
-    
-    for (var i = 0; i < data.length; i++) {
-        distance.push(data[i]);
-    }*/
-    distance = [114, 514]; // test
+    let distance = [];
+    let jsonArray = JSON.parse(test2).values;
+    /*let url = ""; // fill url here
+    fetch(url).then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('無法連絡伺服器，請回報開發者');
+        }).then((json) => {
+            var jsonArray = JSON.parse(json);
+            for (var i = 0; i < jsonArray.length; i++) {
+                distance.push(jsonArray[i]);
+            }
+            return distance;
+        }).catch((error) => {
+            alert(error);
+        });
+    */
+
+    for (var i = 0; i < jsonArray.length; i++) { //test
+        distance.push(jsonArray[i]);
+    }
     return distance;
 }
-
 function displayToilet(toilets, distance) {
     var f = 0;
     if (distance != null && Array.isArray(distance) && distance.length == toilets.length) {
@@ -79,23 +107,30 @@ function displayToilet(toilets, distance) {
         temp.bindPopup(formatPopup(toilets[i], f ? distance[i] : -1));
     }
 }
-
 function formatPopup(toilet, toiletDist) {
     let name = toilet.name;
-    let type = toilet.type;
+    let type;
+    switch (toilet.type) {
+        case "All Gender": type = "不分性別"; break;
+        case "Male": type = "男廁"; break;
+        case "Female": type = "女廁"; break;
+        case "Disabled": type = "無障礙廁所"; break;
+        default: type = toilet.type;
+    }
     let free = toilet.isFree ? "" : "<b>需付費使用</b><br>";
     let available = toilet.Status.isAvailable ? "" : "<b>目前不可用</b><br>";
     let clean = toilet.Status.isClean ? "乾淨整潔" : "整潔欠佳";
     let paper = toilet.Status.isPaper ? "有衛生紙" : "沒有衛生紙";
     let soap = toilet.Status.isSoap ? "有肥皂" : "沒有肥皂";
     let dist = "";
-    if (toiletDist >= 0) dist = "距離：" + toiletDist;
-    var format = "<h2>" + name + "</h2>\
-                    種類：" + type + "<br>\
-                    " + free + available + dist + "<hr>\
-                    " + clean + "<br>\
-                    " + paper + "<br>\
-                    " + soap;
+    if (toiletDist >= 0) dist = "距離：" + toiletDist + "m";
+    var format = "\
+        <h2>" + name + "</h2>\
+        種類：" + type + "<br>\
+        " + free + available + dist + "<hr>\
+        " + clean + "<br>\
+        " + paper + "<br>\
+        " + soap;
     return format;
 }
 
@@ -106,7 +141,6 @@ function findToilet() {
     }
     return navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
 }
-
 function locationSuccess(position) {
     var longitude = position.coords.longitude, latitude = position.coords.latitude;
     markerLayer.getLayers().forEach((item) => {
@@ -118,12 +152,10 @@ function locationSuccess(position) {
     marker.bindPopup("現在位置").openPopup();
     calcDistance();
 }
-
 function calcDistance() {
     var distance = getDistance();
     displayToilet(toiletsAll, distance);
 }
-
 function locationError(err) {
     switch (err.code) {
         case err.PERMISSION_DENIED:
@@ -143,9 +175,108 @@ function locationError(err) {
 }
 
 function addToilet() {
-    //document.getElementById("overlay").style.visibility = visible;
-    alert("功能開發中");
+    document.getElementById("overlay").style.visibility = "visible";
+    document.getElementById("menu").style.visibility = "visible";
+}
+function form() {
+    formLayer.getLayers().forEach((item) => {
+        formLayer.removeLayer(item);
+    });
+    var marker = L.marker(map.getCenter()).addTo(formLayer);
+    marker.bindPopup(makeForm()).openPopup();
+    closeDialog();
+}
+function makeForm() {
+    var format = "\
+        <div class='form'>\
+            <label for='name'>廁所名稱：</label>\
+            <input type='text' id='form_name' name='name'><br>\
+            <label for='type'>廁所種類：</label>\
+            <select id='form_type' name='type'>\
+                <option value='All Gender'>不分性別</option>\
+                <option value='Male'>男廁</option>\
+                <option value='Female'>女廁</option>\
+                <option value='Disabled'>無障礙</option>\
+            </select><br>\
+            <input type='checkbox' id='form_isFree' name='isFree' value='isFree' checked>\
+            <label for='isFree'>免費使用</label>\
+            <input type='checkbox' id='form_isAvailable' name='isAvailable' value='isAvailable' checked>\
+            <label for='isAvailable'>目前可用</label>\
+            <input type='checkbox' id='form_isClean' name='isClean' value='isClean' checked>\
+            <label for='isClean'>乾淨整潔</label><br>\
+            <input type='checkbox' id='form_isPaper' name='isPaper' value='isPaper'>\
+            <label for='isPaper'>有衛生紙</label>\
+            <input type='checkbox' id='form_isSoap' name='isSoap' value='isSoap'>\
+            <label for='isSoap'>有肥皂</label><br>\
+            <input type='text' id='form_latitude' name='name' value="+ map.getCenter().lat + " hidden>\
+            <input type='text' id='form_longitude' name='name' value="+ map.getCenter().lng + " hidden>\
+            <input type='button' value='送出' onclick='submit()'>\
+        </div>";
+    return format;
+}
+function submit() {
+    if(v("form_name") == ""){
+        alert("請輸入廁所名稱");
+        return;
+    }
+    /*
+    let url = ""; // fill url here
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+            name: v(form_name),
+            type: v(form_type),
+            isFree: c(form_isFree),
+            Longitude: v(form_longitude),
+            Latitude: v(form_latitude),
+            Status: {
+                isAvailable: c(form_isAvailable),
+                isClean: c(form_isClean),
+                isPaper: c(form_isPaper),
+                isSoap: c(form_isSoap)
+            }
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }).then((response) => {
+        if (response.ok) {
+            refresh();
+        }
+        throw new Error('無法連絡伺服器，請回報開發者');
+    }).catch((error) => {
+        alert(error);
+    });*/
+    test3.push(new Toilet().set({ //test
+        name: v("form_name"),
+        type: v("form_type"),
+        isFree: c("form_isFree"),
+        Longitude: v("form_longitude"),
+        Latitude: v("form_latitude"),
+        Status: {
+            isAvailable: c("form_isAvailable"),
+            isClean: c("form_isClean"),
+            isPaper: c("form_isPaper"),
+            isSoap: c("form_isSoap")
+        }
+    }));
+    refresh(); //test
+}
+function v(id){
+    return document.getElementById(id).value;
+}
+function c(id){
+    return document.getElementById(id).checked;
 }
 function closeDialog() {
-    document.getElementById("overlay").style.visibility = hidden;
+    document.getElementById("overlay").style.visibility = "hidden";
+    document.getElementById("menu").style.visibility = "hidden";
+}
+function refresh(){
+    alert('資料上傳成功');
+    formLayer.getLayers().forEach((item) => {
+        formLayer.removeLayer(item);
+    });
+    getToiletLocation();
+    displayToilet(test3, null); //test
 }

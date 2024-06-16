7fd9f15a68fcef6e2bda74e51dc6f563c08e0798
Winston <winstonlaipengfeng@gmail.com>
2024-06-16 09:59:27 +0800

    Combined with Status class

--- src/main/java/com/example/toilet/Location.java
+++ src/main/java/com/example/toilet/Location.java
@@ -4,22 +4,28 @@ public class Location {
     private String name;
     private String type;
     private boolean isFree;
-    private String latitude;
     private String longitude;
-    private Status status;
+    private String latitude;
+    private boolean isAvailable;
+    private boolean isClean;
+    private boolean isPaper;
+    private boolean isSoap;
 
-    public Location() {
-    }
-
-    public Location(String name, String type, boolean isFree, String latitude, String longitude, Status status) {
+    // Constructors
+    public Location(String name, String type, boolean isFree, String longitude, String latitude,
+                    boolean isAvailable, boolean isClean, boolean isPaper, boolean isSoap) {
         this.name = name;
         this.type = type;
         this.isFree = isFree;
-        this.latitude = latitude;
         this.longitude = longitude;
-        this.status = status;
+        this.latitude = latitude;
+        this.isAvailable = isAvailable;
+        this.isClean = isClean;
+        this.isPaper = isPaper;
+        this.isSoap = isSoap;
     }
 
+    // Getters and Setters
     public String getName() {
         return name;
     }
@@ -36,20 +42,12 @@ public class Location {
         this.type = type;
     }
 
-    public boolean getIsFree() {
+    public boolean isFree() {
         return isFree;
     }
 
-    public void setFree(boolean isFree) {
-        this.isFree = isFree;
-    }
-
-    public String getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(String latitude) {
-        this.latitude = latitude;
+    public void setFree(boolean free) {
+        isFree = free;
     }
 
     public String getLongitude() {
@@ -60,11 +58,59 @@ public class Location {
         this.longitude = longitude;
     }
 
-    public Status getStatus() {
-        return status;
+    public String getLatitude() {
+        return latitude;
     }
 
-    public void setStatus(Status status) {
-        this.status = status;
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public boolean isClean() {
+        return isClean;
+    }
+
+    public void setClean(boolean clean) {
+        isClean = clean;
+    }
+
+    public boolean isPaper() {
+        return isPaper;
+    }
+
+    public void setPaper(boolean paper) {
+        isPaper = paper;
+    }
+
+    public boolean isSoap() {
+        return isSoap;
+    }
+
+    public void setSoap(boolean soap) {
+        isSoap = soap;
+    }
+
+    // toString method (for logging and debugging)
+    @Override
+    public String toString() {
+        return "Location{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", isFree=" + isFree +
+                ", longitude=" + longitude +
+                ", latitude=" + latitude +
+                ", isAvailable=" + isAvailable +
+                ", isClean=" + isClean +
+                ", isPaper=" + isPaper +
+                ", isSoap=" + isSoap +
+                '}';
     }
 }
